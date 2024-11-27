0b71493ee876f2a5f18ca52e7e6477a5bf214d3a
Lucasfdp <lucasfdpinedo@gmail.com>
2024-11-27 20:54:49 +0100

    Add files via upload

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luferna3 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 19:32:26 by luferna3          #+#    #+#             */
+/*   Updated: 2024/11/26 12:42:45 by luferna3         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	destlen;
+	unsigned int	srclen;
+	unsigned int	i;
+
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	i = 0;
+	if (size <= destlen)
+	{
+		return (size + srclen);
+	}
+	while (src[i] != '\0' && destlen + i < size - 1)
+	{
+		dest[destlen + i] = src[i];
+		i++;
+	}
+	dest[destlen + i] = '\0';
+	return (destlen + srclen);
+}
